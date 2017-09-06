45ea08566a0217f02e66fcf65276eff525dae851
Pavel Gushcha <pavimus@gmail.com>
2017-09-06 09:51:14 +0300

    refactor api calling

--- services/RocketSmsBy.php
+++ services/RocketSmsBy.php
@@ -7,15 +7,35 @@ class RocketSmsBy extends \pavimus\sms\Service {
     public $username;
     public $password;
 
-    public function send($phone, $text, $priority, &$smsId) {
-        $curl = curl_init();
-
-        curl_setopt($curl, CURLOPT_URL, 'http://api.rocketsms.by/json/send');
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_POST, true);
-        $params=[
+    private function callApi($method,$params) {
+        $params=array_merge($params,[
             'username'=>$this->username,
             'password'=>$this->password,
+        ]);
+
+        $curl = curl_init();
+
+        curl_setopt($curl, CURLOPT_URL, 'http://api.rocketsms.by/json/'.$method);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($params));
+
+        $result = @json_decode(curl_exec($curl), true);
+
+        if (!$result) {
+            throw new \Exception('RocketSMS.by api error');
+        }
+
+        if ($result && isset($result['error'])) {
+            throw new \Exception("RocketSMS.by api error, ErrorID=" . $result['error']);
+        }
+
+        return $result;
+    }
+
+    public function send($phone, $text, $priority, &$smsId) {
+        $params=[
             'phone'=>$phone,
             'text'=>$text
         ];
@@ -24,22 +44,13 @@ class RocketSmsBy extends \pavimus\sms\Service {
             $params['priority']='true';
         }
 
-        $encodedParams=[];
-        foreach($params as $k=>$v) {
-            $encodedParams[]="$k=".rawurlencode($v);
-        }
-
-        curl_setopt($curl, CURLOPT_POSTFIELDS, implode('&',$encodedParams));
-
-        $result = @json_decode(curl_exec($curl), true);
-
-        if ($result && isset($result['id'])) {
+        try {
+            $result=$this->callApi('send',$params);
             $smsId=$result['id'];
-            return true;
-        } elseif ($result && isset($result['error'])) {
-            return "Error occurred while sending message. ErrorID=" . $result['error'];
-        } else {
-            return "Service error";
+        } catch (\Exception $e) {
+            return $e->getMessage();
         }
+
+        return true;
     }
 }
